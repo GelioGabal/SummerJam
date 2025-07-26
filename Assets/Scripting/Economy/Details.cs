1f96c12f000b14a1e2983636642aff82fefee12b
GelioGabal <stephan906082@gmail.com>
2025-07-26 20:56:43 +0300

    Details system full

--- Assets/Scripting/Economy/Details.cs
+++ Assets/Scripting/Economy/Details.cs
@@ -1,17 +1,20 @@
 using UnityEngine;
+using UnityEngine.Events;
 
 public class Details : MonoBehaviour
 {
     private static int _detailsCount = 0;
+    public static event System.Action<int> OnDetailsChanged; //
 
-    public  int GetDetailsCount()
+    public static int GetDetailsCount()
     {
         return _detailsCount;
     }
 
-
-    public  void SetDetailsCount(int newValue)
+    public static void SetDetailsCount(int newValue)
     {
+        int oldValue = _detailsCount;
+
         if (newValue < 0)
         {
             Debug.LogWarning("Нельзя установить значение меньше 0!");
@@ -26,5 +29,13 @@ public class Details : MonoBehaviour
         {
             _detailsCount = newValue;
         }
+
+ 
+        if (oldValue != _detailsCount)
+        {
+            OnDetailsChanged?.Invoke(_detailsCount);
+        }
     }
+
+
 }
