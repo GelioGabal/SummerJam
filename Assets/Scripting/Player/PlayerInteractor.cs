705e9559655e1831f83805590f2963aeb3190971
SerugeiInSyberia <serugei.alt@mail.ru>
2025-07-27 02:17:28 +0300

    New Interaction

---
+++ Assets/Scripting/Player/PlayerInteractor.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerInteractor : MonoBehaviour
+{
+    InteractiveObject curInteract;
+    void Start()
+    {
+        InputManager.playerInput.Object.Interact.performed += Interact;
+    }
+    private void OnDisable()
+    {
+        InputManager.playerInput.Object.Interact.performed -= Interact;
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.TryGetComponent(out curInteract)) 
+            curInteract.ShowTooltip(true);
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.TryGetComponent(out curInteract))
+        {
+            curInteract.ShowTooltip(false);
+            curInteract = null;
+        }
+    }
+    void Interact(InputAction.CallbackContext ctx)
+    {
+        curInteract?.OnInteract.Invoke();
+    }
+}
